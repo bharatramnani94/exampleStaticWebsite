0e507b47d724e07a19e5fbf546202bd92bdebd41
Bharat Ramnani <bharatramnani94@gmail.com>
2020-07-22 00:15:32 +0530

    Refactor things and make detail page dynamic

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,7 +1,28 @@
-const urlParams = new URLSearchParams(window.location.search);
-const title = urlParams.get('title') || "";
-const tag = urlParams.get('tag') || "";
-const img = urlParams.get('img') || "";
-console.log({
-  title, tag, img,
-})
+function populateData({imageName, title, desc} = experience) {
+  const titleEl = document.querySelector("#js--title");
+  const descEl = document.querySelector("#js--desc");
+  const imageEl = document.querySelector("#js--image");
+  titleEl.textContent = title;
+  descEl.textContent = desc;
+  imageEl.setAttribute("src", `./images/${imageName}`);
+}
+
+function getIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id') || "1";
+  return id;
+}
+
+function findExperienceById(id) {
+  const allExperiences = JSON.parse(window.localStorage.getItem("[Experiences]AllExperiences"));
+  const activeExperience = allExperiences.find(e => e.id === id);
+  return activeExperience || allExperiences[0];
+}
+
+function main() {
+  const id = getIdFromUrl();
+  const activeExperience = findExperienceById(id);
+  populateData(activeExperience);
+}
+
+main();
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,39 @@
-console.log("Hello World");
+const experiences = [
+  {
+    id: "1",
+    title: "Cave and Canyon Trail",
+    tag: "Featured",
+    imageName: "earth-1.jpg",
+    desc: `Go to Las Vegas and check out the epic Canyon trails and discover some awesome hidden caves.`,
+  },
+  {
+    id: "2",
+    title: "Cross the mountains",
+    tag: "Sky",
+    imageName: "earth-3.jpg",
+    desc: `Challenge yourself physically and mentally and reach "the steps to heaven".`,
+  },
+  {
+    id: "3",
+    title: "Ride the shark waves",
+    tag: "Experiences",
+    imageName: "water-7.jpg",
+    desc: `We'll take you to Hawaii where you can live a lifelong dream many people have - surfing among the sharks.`,
+  },
+  {
+    id: "4",
+    title: "Underwater Exploration",
+    tag: "Water",
+    imageName: "water-4.jpg",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque voluptate quod corporis tenetur labore obcaecati eius vitae error amet illum.`,
+  },
+  {
+    id: "5",
+    title: "Tropical Getaway",
+    tag: "Earth",
+    imageName: "earth-6.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae autem cumque accusantium est delectus hic laboriosam odit quisquam, sapiente tempore.`,
+  },
+]
+
+window.localStorage.setItem("[Experiences]AllExperiences", JSON.stringify(experiences));
